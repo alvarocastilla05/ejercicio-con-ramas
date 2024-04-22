d6b88ec8576ae31662d539c59c718bf3a67dc7d5
AlvaroCastilla <castilla.caalv24@triana.salesianos.edu>
2024-04-22 16:46:48 +0200

    Cambios en la rama

--- EjercicioRamas/src/main/java/com/salesianostriana/dam/ejercicioramas/MainDeMentira.java
+++ EjercicioRamas/src/main/java/com/salesianostriana/dam/ejercicioramas/MainDeMentira.java
@@ -1,5 +1,20 @@
 package com.salesianostriana.dam.ejercicioramas;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import jakarta.annotation.PostConstruct;
+
+@Component
 public class MainDeMentira {
 
+	@Autowired
+	private Animador animador;
+	
+	@PostConstruct
+	public void init() {
+		
+		animador.imprimirMensajeDeAnimo();
+	}
+	
 }
